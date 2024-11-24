ff0f2caf855187ec44e4077095024dd1f20d9af4
xingyu liu <2386131161@qq.com>
2024-11-24 20:02:45 +0800

    Initial commit

---
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_TCPServer_FILE D:/QTWorkSpace/TCPServer/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/TCPServer.exe)
+set(__QT_DEPLOY_TARGET_TCPServer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TCPServer_RUNTIME_DLLS D:/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;D:/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
